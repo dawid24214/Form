4abdd0c3d118f8d73cf44577d210883d66b793c6
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-03-21 09:21:26 +0100

    form_jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,29 @@ import './App.css'
 function App() {
   return (
     <>
+      <div>
+        <h2>Skontaktuj się z  nami </h2>
+        <form className='form-group'>
+
+          <div className='form-group'>
+          <label form='name'>Imie i nazwisko </label>
+          <input typeof='text' id='name' name='name' placeholder='Twoje imie i nazwisko...' required/>
+          </div>
+          <div className='form-group'>
+          <label form='emial'>Adres e-mail</label>
+          <input typeof='email' id='email' name='email' placeholder='Podaj swoj adres email...'  required/>
+          </div>
+          <div className='form-group'>
+          <label typeof='message'>Wiadomość</label>
+          <textarea id='message' name='message' placeholder='Napisz swoją wiadomość...' required/>
+            </div>
+          <div className='form-group'>
+            <button typeof='submit'>Wyślij</button>
+          </div>
+        </form>
+
+
+      </div>
 
     </>
   )
