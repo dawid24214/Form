0b40d602d712129a738536a8a5ddd2fc30d2060b
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-03-24 21:30:29 +0100

    Validation
    improved the form : fixed HTML , added data handling validation and form reset after submit .

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,92 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [formData, setFormData] = useState({
+
+    name: '',
+    email:'',
+    message: ''
+
+  });
+  const handleChange = (e) =>{
+    const {name,value} = e.target;
+    setFormData(prev =>({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    if (!formData.name || !formData.email || !formData.message){
+      alert('Wszystkie pola są wymagane!');
+      return;
+    }
+    console.log('Formularz został wysłany:', formData);
+    alert('dziekuję za kontakt !');
+
+    setFormData({
+      name: '',
+      email: '',
+      message: ''
+    });
+  };
+
+
+
+
   return (
     <>
-      <div>
-        <h2>Skontaktuj się z  nami </h2>
-        <form className='form-group'>
+      <div className='form-container'>
+        <h2>Skontkatuj się z nami!</h2>
+
+        <form className='form-group' onSubmit={handleSubmit}>
+          <div className='form-group'>
+            <label htmlFor='name'>Imie i nazwisko</label>
+            <input
+            type='text'
+            id='name'
+            name='name'
+            placeholder='Twoje imie i nazwisko ...'
+            value={formData.name}
+            onChange={handleChange}
+            required
+            />
+          </div>
 
           <div className='form-group'>
-          <label form='name'>Imie i nazwisko </label>
-          <input typeof='text' id='name' name='name' placeholder='Twoje imie i nazwisko...' required/>
+
+            <label htmlFor='email'>Podaj email...</label>
+            <input
+            type='email'
+            id='email'
+            name='email'
+            placeholder='Twój adres email.'
+            value={formData.email}
+            onChange={handleChange}
+            required
+            />
           </div>
           <div className='form-group'>
-          <label form='emial'>Adres e-mail</label>
-          <input typeof='email' id='email' name='email' placeholder='Podaj swoj adres email...'  required/>
+            <label htmlFor='message'>Wiadomość</label>
+            <textarea
+            id='message'
+            name='message'
+            placeholder='Napisz wiadomość'
+            value={formData.message}
+            onChange={handleChange}
+            required
+            />
           </div>
+
           <div className='form-group'>
-          <label typeof='message'>Wiadomość</label>
-          <textarea id='message' name='message' placeholder='Napisz swoją wiadomość...' required/>
-            </div>
-          <div className='form-group'>
-            <button typeof='submit'>Wyślij</button>
+            <button type='submit'>Wyślij</button>
           </div>
+
+
         </form>
+
 
 
       </div>
